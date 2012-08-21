62de9336752780fe63854fe6c9adfda083757278
Андрей Гурьев <andrey@kidberries.com>
2012-08-21 13:51:04 +0400

    Start

---
+++ skin/frontend/kidberries/default/js/cufon-replace.js
@@ -0,0 +1,13 @@
+Cufon.replace("button.button span span, #nav > li > .menu-button-layer > a > span, .block .block-title span", { fontFamily: 'MyriadPro' });
+Cufon.replace(".products-grid .price-box .price, .products-list .price-box .price, .product-view .price-box .price", { fontFamily: 'MyriadPro' });
+Cufon.replace(".error-msg, .success-msg, .note-msg, .notice-msg", { fontFamily: 'MyriadPro' });
+
+Cufon.replace(".header .phone div", { fontFamily: 'Calibri' });
+  Cufon.replace(".product-view .product-shop .status .availability span", { fontFamily: 'Calibri' });
+Cufon.replace(".product-view .product-shop .add-to-cart label", { fontFamily: 'Calibri' });
+/*
+jQuery(document).ready(function() {
+  jQuery(".header .logo .text .label, .header .logo .text .slogan, .header table.phone tr td").hide();
+  jQuery(".header .logo .text .label, .header .logo .text .slogan, .header table.phone tr td").show("slow");
+});
+*/
---
+++ skin/frontend/kidberries/default/js/menu.js
@@ -0,0 +1,5 @@
+jQuery('#nav > li').each(function(index){
+	jQuery(this).addClass('menu-button-'+(index+1));
+	jQuery(this).find('a').first().wrap('<div class="menu-button-layer" />');
+	jQuery(this).find('span').first().wrapInner('<b />');
+});
